f6c7158d6acd253dfda82715ee4b3fa4057d99c2
SeanSyue <sean962081468@gmail.com>
2019-02-04 23:16:21 +0800

    init commit

---
+++ src/main/java/com/example/gsmysql/GsMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.gsmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsMysqlApplication.class, args);
+    }
+
+}
+
