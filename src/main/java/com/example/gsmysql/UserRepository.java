471ecd5d8002272ca0b703d75ce55a3cd9db550f
SeanSyue <sean962081468@gmail.com>
2019-02-05 16:54:53 +0800

    use jpa repository

--- src/main/java/com/example/gsmysql/UserRepository.java
+++ src/main/java/com/example/gsmysql/UserRepository.java
@@ -1,12 +1,12 @@
 package com.example.gsmysql;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.gsmysql.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
